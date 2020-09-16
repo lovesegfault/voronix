9871d965b1a71a3704147193bcca7ce62e6bb37b
Bernardo Meurer <bernardo@meurer.org>
2020-09-16 10:57:52 -0700

    klipper: remove nondeterminism

--- klipper.nix
+++ klipper.nix
@@ -24,11 +24,15 @@
     ++ lib.optional armSupport gcc-arm-embedded
     ++ lib.optional avrSupport pkgsCross.avr.stdenv.cc;
 
+  postPatch = ''
+    sed -i 's/return version/return "${version}"/g' ./scripts/buildcommands.py
+  '';
+
   preBuild = lib.optionalString (configFile != null) "cp ${configFile} ./.config";
 
   installPhase = ''
     mkdir -p $out
-    cp out/klipper.bin $out || true
-    cp out/klipper.elf $out || true
+    cp ./.config $out/config
+    cp out/klipper.bin $out
   '';
 }
