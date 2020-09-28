9f819ff15384aa154d533e5df551b14764f9dd2b
Bernardo Meurer <bernardo@meurer.org>
2020-09-27 18:06:03 -0700

    klipper: update and fix cfg

--- klipper.nix
+++ klipper.nix
@@ -16,8 +16,8 @@
   src = fetchFromGitHub {
     owner = "KevinOConnor";
     repo = "klipper";
-    rev = "3ac636b33e0dd770d2608d9a7c05c267f1b12149";
-    sha256 = "1wwggsrlzhjik2gglkkxg7a3fyvhzpmkzhd5g3szjk8gsww83kha";
+    rev = "4d0d219716afb1143119d7e919fa1e896a896f11";
+    sha256 = "02xv1a31ln3zkcx240c3qkfmglg1cnhdcanxv44ryvnd9fr854sj";
   };
 
   buildInputs = [ python2 ]
